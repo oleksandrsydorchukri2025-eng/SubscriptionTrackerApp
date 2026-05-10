ad7d9ab8bd8bf12cf28374b7a31e93d8159d6e87
oleksandrsydorchukri2025-eng <oleksandr.sydorchuk.2025@gmail.com>
2026-05-11 00:18:31 +0300

    Fixed: added internet permission and updated BASE_URL for Render

--- app/src/main/java/com/example/subscriptiontracker/api/RetrofitClient.kt
+++ app/src/main/java/com/example/subscriptiontracker/api/RetrofitClient.kt
@@ -9,6 +9,7 @@ object RetrofitClient {
     private const val BASE_URL = "https://kolo-backend-api.onrender.com/"
     val apiService: ApiService by lazy {
         Retrofit.Builder()
+
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
